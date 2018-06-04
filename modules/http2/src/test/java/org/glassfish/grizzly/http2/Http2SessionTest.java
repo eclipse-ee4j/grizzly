eeddc98a73e5ecb4f09e76210eda14fbe333b80c
Sven Diedrichsen <sdiedrichsen@yahoo.de>
2018-06-04 22:39:13 +0200

    Refactored test
    Signed-off-by: Sven Diedrichsen <sdiedrichsen@yahoo.de>

--- modules/http2/src/test/java/org/glassfish/grizzly/http2/Http2SessionTest.java
+++ modules/http2/src/test/java/org/glassfish/grizzly/http2/Http2SessionTest.java
@@ -10,46 +10,58 @@ import org.glassfish.grizzly.memory.ByteBufferManager;
 import org.glassfish.grizzly.memory.MemoryManager;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 /**
  * Test {@link Http2Session}
- * @author sdiedrichsen
- * @version $Id$
+ * @author Sven Diedrichsen (sven.diedrichsen@gmail.com)
  * @since 26.05.18
  */
 public class Http2SessionTest {
 
-    @Mock
-    private Connection<?> connection;
-    @Mock
-    private Http2Configuration configuration;
-    @Mock
-    private Filter filter;
+    private Http2Configuration configuration =
+            Http2Configuration.builder()
+                .cleanPercentage(1.0f)
+                .streamsHighWaterMark(0.01f).build();
 
     private Http2Session session;
 
     @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        MemoryManager memoryManager = new ByteBufferManager();
-        FilterChain filterChain = new DefaultFilterChain();
-        filterChain.add(filter);
-        doReturn(filterChain).when(connection).getProcessor();
-        doReturn(memoryManager).when(connection).getMemoryManager();
-        doReturn(0.01f).when(configuration).getStreamsHighWaterMark();
-        doReturn(1.0f).when(configuration).getCleanPercentage();
-        Http2BaseFilter handlerFilter = new Http2ServerFilter(configuration);
-        session = new Http2Session(connection, true, handlerFilter);
+    public void setUp() {
+        FilterChain filterChain = newFilterChain();
+        Connection<?> connection = newConnectionMock(filterChain);
+        session = new Http2Session(
+            connection,
+            true,
+            new Http2ServerFilter(configuration)
+        );
+        session.setupFilterChains(
+            newFilterChainContext(filterChain, connection),
+            false
+        );
+    }
+
+    private FilterChainContext newFilterChainContext(FilterChain filterChain, Connection<?> connection) {
         FilterChainContext context = FilterChainContext.create(connection);
         context.getInternalContext().setProcessor(filterChain);
         context.setFilterIdx(0);
         context.setEndIdx(-1);
-        session.setupFilterChains(context, false);
+        return context;
+    }
+
+    private FilterChain newFilterChain() {
+        FilterChain filterChain = new DefaultFilterChain();
+        filterChain.add(mock(Filter.class));
+        return filterChain;
+    }
+
+    private Connection<?> newConnectionMock(FilterChain filterChain) {
+        Connection<?> connection = mock(Connection.class);
+        MemoryManager memoryManager = new ByteBufferManager();
+        doReturn(filterChain).when(connection).getProcessor();
+        doReturn(memoryManager).when(connection).getMemoryManager();
+        return connection;
     }
 
     @Test
@@ -59,6 +71,10 @@ public class Http2SessionTest {
         session.registerStream(1, stream);
         session.registerStream(2, stream);
         session.registerStream(3, stream);
+
         session.terminate(ErrorCode.INTERNAL_ERROR, "Test");
+
+        verify(stream, times(3)).closedRemotely();
     }
+
 }
