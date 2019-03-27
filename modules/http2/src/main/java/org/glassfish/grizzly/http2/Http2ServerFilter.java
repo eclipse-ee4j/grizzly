c762fcd8044b25209efb862784a8588cc7d4ce88
Matt Gill <matthew.gill@live.co.uk>
2019-03-27 09:14:42 +0000

    Fixed copyright headers.
    Signed-off-by: Matt Gill <matthew.gill@live.co.uk>

--- modules/http2/src/main/java/org/glassfish/grizzly/http2/Http2ServerFilter.java
+++ modules/http2/src/main/java/org/glassfish/grizzly/http2/Http2ServerFilter.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2015, 2019 Oracle and/or its affiliates and others
+ * Copyright (c) 2015, 2019 Oracle and/or its affiliates and others.
+ * All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
