627b53f7766f2b05f4c7dfbd32121dbaf9f9e7bc
David Matejcek <dmatej@seznam.cz>
2020-11-18 13:23:10 +0100

    Issue #2016 Prevent HTTP/2 push when globally disabled
    Streams still allowed pushing resources when globally disabled as the
    stream itself and the session both allow it. This change makes the
    stream also respect the global configuration.
    
    Signed-off-by: Matthew Gill <matthew.gill@live.co.uk>
    Signed-off-by: David Matějček <dmatej@seznam.cz>

--- modules/http2/src/main/java/org/glassfish/grizzly/http2/Http2Session.java
+++ modules/http2/src/main/java/org/glassfish/grizzly/http2/Http2Session.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2012, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2020 Oracle and/or its affiliates and others.
+ * All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -489,7 +490,7 @@ public class Http2Session {
      * Push is enabled by default.
      */
     public boolean isPushEnabled() {
-        return pushEnabled;
+        return pushEnabled && http2Configuration.isPushEnabled();
     }
 
     /**
